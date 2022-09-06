c6e44879688a08e51e02a3c2a9e3341ecf387657
Zaki Khalid <zaki.khalid@bykea.com>
2022-09-06 09:40:52 +0500

    file upload

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
+import * as React from "react";
+import uuid from 'react-uuid';
+import { Dropzone, FileItem } from "@dropzone-ui/react";
 
-function FileUpload() {
+import api from '../Services/ParentControlService';
 
-    const [file, setFile] = useState()
+const _token = localStorage.getItem("token");
 
-    function handleChange(event) {
-        setFile(event.target.files[0])
-        console.log(file);
-    }
+const FileUpload = () => {
+    const [files, setFiles] = React.useState([]);
+    const updateFiles = (incommingFiles) => {
+        setFiles(incommingFiles);
+        const uploadFile = async () => {
+            try {
+                const data = await api.post("v1/add/file", incommingFiles[0].file, {
+                    headers: {
+                        authorization: 'Bearer '.concat(_token),
+                        "Access-Control-Allow-Origin": "*",
+                        'Content-Type': 'text/plain'
+                    },
+                });
 
+                console.log(data);
+                return data;
+            }
+            catch (err) {
+                console.log(err);
+            }
+            return 0;
+        }
+
+        uploadFile();
+    };
     return (
-        <div className="App">
-            <form>
-                <h1>React File Upload</h1>
-                <input type="file" onChange={handleChange} />
-                <button type="submit">Upload</button>
-            </form>
-        </div>
+        <>
+            <h2>Upload new file to system</h2>
+            <Dropzone onChange={updateFiles} value={files} maxFiles={1} style={{ width: "40%", margin: "2em auto" }} >
+                {files.map((file) => (
+                    <FileItem {...file} preview key={uuid()} />
+                ))}
+            </Dropzone>
+        </>
     );
 }
 
--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -4,24 +4,14 @@
 import { Container, Typography } from '@mui/material';
 // components
 import FileUpload from '../components/FileUpload';
+
 import Table from './Table';
 import Page from '../components/Page';
 import User from './User';
-// import Iconify from '../components/Iconify';
-// // sections
-// import {
-//   AppTasks,
-//   AppNewsUpdate,
-//   AppOrderTimeline,
-//   AppCurrentVisits,
-//   AppWebsiteVisits,
-//   AppTrafficBySite,
-//   AppWidgetSummary,
-//   AppCurrentSubject,
-//   AppConversionRates,
-// } from '../sections/@dashboard/app';
 
 // ----------------------------------------------------------------------
+
+const _role = localStorage.getItem("role");
 
 export default function DashboardApp() {
   // const theme = useTheme();
@@ -29,10 +19,10 @@ export default function DashboardApp() {
   return (
     <Page title="Dashboard">
       <Container maxWidth="xl">
-        <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, Welcome back
+        <Typography variant="h4" sx={{ mb: 5 }} style={{ textAlign: "center" }}>
+          Hi, Welcome to PCS
         </Typography>
-        <FileUpload />
+        {_role === "admin" && <FileUpload />}
         <Table />
         <User />
       </Container>
